46c54b89511b9df5787a9aedd2251e8ac2428162
umg026 <umangwork26@gmail.com>
2024-12-01 19:40:14 +0530

    fronend chnage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'; // Correct imports for React Router v5
-import Home from './routes/Home';
-import Chat from './routes/chat';
+import Home from './component/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/chat" exact component={Chat} />
+        {/* <Route path="/chat" exact component={Chat} /> */}
 
       </Switch>
     </BrowserRouter>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,9 @@
-import { Provider } from "@/components/ui/provider"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <Provider>
         <App />
-      </Provider>
   </React.StrictMode>,
 )
